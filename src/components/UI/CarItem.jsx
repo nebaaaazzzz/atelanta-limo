dccf1819adae3e4bf446d137ca1f8630837ad211
Nebiyu <60134585+nebaaaazzzz@users.noreply.github.com>
2023-06-21 16:23:01 +0300

    Update CarItem.jsx

--- src/components/UI/CarItem.jsx
+++ src/components/UI/CarItem.jsx
@@ -17,21 +17,9 @@ const CarItem = (props) => {
 
         <div className="car__item-content mt-4">
           <h4 className="section__title text-center">{name}</h4>
-          <h6 className="rent__price text-center mt-">
-            ${pricePerDay}.00 <span>/ Day</span>
-          </h6>
 
           <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
-            <span className=" d-flex align-items-center gap-1">
-              <i class="ri-car-line"></i> {model}
-            </span>
-            <span className=" d-flex align-items-center gap-1">
-              <i class="ri-settings-2-line"></i>{" "}
-              {automatic ? "automatic" : "manual"}
-            </span>
-            <span className=" d-flex align-items-center gap-1">
-              <i class="ri-timer-flash-line"></i> {speed}
-            </span>
+           
           </div>
 
           <button
