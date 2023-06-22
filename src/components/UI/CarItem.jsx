0d0902468cdf3e2becac6003ca11de183a5855cc
Yosef781227 <yosefnakachew101@gmail.com>
2023-06-22 19:03:07 +0300

    a

--- src/components/UI/CarItem.jsx
+++ src/components/UI/CarItem.jsx
@@ -18,7 +18,7 @@ const CarItem = (props) => {
         <div className="car__item-content mt-4">
           <h4 className="section__title" >{name}</h4> 
           <div className="car__item-info d-flex  justify-content-between mt-3 mb-4">
-            <h2 className="section__title " >Price Per Day : {pricePerDay}$</h2>
+            <h1 >Price Per Day : {pricePerDay}$</h1>
             
           </div>
 
