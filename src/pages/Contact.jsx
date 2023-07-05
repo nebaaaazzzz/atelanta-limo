95b75d97972dcbf45e55ddc6a5ac2173360cbcde
Yosef781227 <yosefnakachew101@gmail.com>
2023-07-05 15:12:17 +0300

    required

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,6 @@ const socialLinks = [
     url: "#",
     icon: "ri-instagram-line",
   },
-
   {
     url: "#",
     icon: "ri-twitter-line",
@@ -29,42 +28,51 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+
   const sendEmail = async (e) => {
     e.preventDefault();
-    await contactFormMail({ name, email, phone, message });
-    // emailjs
-    //   .sendForm(
-    //     "service_er3t4rd",
-    //     "template_l8ral8f",
-    //     form.current,
-    //     "jCA_n2fcS5PuK4pQb"
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //
-    //     },
-    //     (error) => {
-    //       alert(
-    //         "Sorry, your message could not be sent. Please try again later."
-    //       );
-    //     }
-    //   );
-    toast.success("Message sent, We will contact you shortly", {
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 5000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-    form.current.reset();
-    setName("");
-    setEmail("");
-    setPhone("");
-    setMessage("");
-    
+    if (validateForm()) {
+      await contactFormMail({ name, email, phone, message });
+      toast.success("Message sent, We will contact you shortly", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      form.current.reset();
+      setName("");
+      setEmail("");
+      setPhone("");
+      setMessage("");
+      setFormErrors({});
+    }
+  };
+
+  const validateForm = () => {
+    let errors = {};
+    let formIsValid = true;
+    if (!name) {
+      formIsValid = false;
+      errors["name"] = "*Please enter your name.";
+    }
+    if (!email) {
+      formIsValid = false;
+      errors["email"] = "*Please enter your email.";
+    }
+    if (!phone) {
+      formIsValid = false;
+      errors["phone"] = "*Please enter your phone number.";
+    }
+    if (!message) {
+      formIsValid = false;
+      errors["message"] = "*Please enter your message.";
+    }
+    setFormErrors(errors);
+    return formIsValid;
   };
 
   return (
@@ -75,7 +83,6 @@ const Contact = () => {
           <Row>
             <Col lg="7" md="7">
               <h6 className="fw-bold mb-4">Get In Touch</h6>
-
               <form ref={form} onSubmit={sendEmail}>
                 <FormGroup className="contact__form">
                   <Input
@@ -87,6 +94,7 @@ const Contact = () => {
                     type="text"
                     name="user_name"
                   />
+                  <div  className="errorMsg" style={{color: 'red'}}>{formErrors.name}</div>
                 </FormGroup>
                 <FormGroup className="contact__form">
                   <Input
@@ -98,15 +106,19 @@ const Contact = () => {
                     type="email"
                     name="user_email"
                   />
+                  <div className="errorMsg" style={{color: 'red'}}>{formErrors.email}</div>
                 </FormGroup>
-                <FormGroup
-                  value={phone}
-                  onChange={(e) => {
-                    setPhone(e.target.value);
-                  }}
-                  className="contact__form"
-                >
-                  <Input placeholder="Phone" type="text" name="user_phone" />
+                <FormGroup className="contact__form">
+                  <Input
+                    value={phone}
+                    placeholder="Phone"
+                    onChange={(e) => {
+                      setPhone(e.target.value);
+                    }}
+                    type="text"
+                    name="user_phone"
+                  />
+                  <div className="errorMsg" style={{color: 'red'}}>{formErrors.phone}</div>
                 </FormGroup>
                 <FormGroup className="contact__form">
                   <textarea
@@ -119,12 +131,13 @@ const Contact = () => {
                     className="text_area"
                     name="message"
                   ></textarea>
+                  <div  className="errorMsg" style={{color: 'red'}}>{formErrors.message}</div>
                 </FormGroup>
                 <input className="contact__btn" type="submit" value="Send" />
               </form>
             </Col>
-
             <Col lg="5" md="5">
+                 <Col lg="5" md="5">
               <div className="contact__info">
                 <h6 className="fw-bold">Contact Information</h6>
                 <p className="section__description mb-0">
@@ -156,6 +169,7 @@ const Contact = () => {
                 </div>
               </div>
             </Col>
+            </Col>
           </Row>
         </Container>
       </section>
@@ -164,3 +178,7 @@ const Contact = () => {
 };
 
 export default Contact;
+
+
+
+
